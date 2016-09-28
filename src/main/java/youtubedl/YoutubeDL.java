b1bdf17c64b557ed3091c1ba5d4eede237dab1d1
1997mattmcc <1997mattmcc@gmail.com>
2016-09-29 03:51:38 +0930

    Simplified YoutubeDL and made the Executable option global

--- src/main/java/youtubedl/YoutubeDL.java
+++ src/main/java/youtubedl/YoutubeDL.java
@@ -1,101 +1,79 @@
 package youtubedl;
 
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Objects;
 import java.util.List;
 
-public class YoutubeDL implements IYoutubeDL {
+public final class YoutubeDL implements Runnable {
 
+    public static final Consumer<List<String>> DEFAULT_TERMINATION_LOGGER = consoleLog -> System.out.println(String.format("ConsoleLog = [ %s ] lines", consoleLog.size()));
+    public static final Consumer<String> DEFAULT_CONSOLE_LOGGER = consoleLine -> System.out.println(String.format("ConsoleLine =  [ %s ]", consoleLine));
+    public static final Consumer<String> DEFAULT_INITIATION_LOGGER = command -> System.out.println(String.format("Command = [ %s ]", command));
+
+    private static String executable = "youtube-dl";
+
+    private final EnumSet<Properties> properties = EnumSet.noneOf(Properties.class);
+    private final EnumMap<Options, String> options = new EnumMap(Options.class);
     private final Consumer<List<String>> terminationLogger;
-    private final EnumMap<Options, String> options;
+    private final List<String> urls = new ArrayList<>();
     private final Consumer<String> consoleLogger;
-    private final EnumSet<Properties> properties;
-    private final String downlaoder;
-    private final List<String> urls;
 
     public YoutubeDL(YoutubeDLBuilder builder) {
         this.terminationLogger = builder.terminationLogger;
         this.consoleLogger = builder.consoleLogger;
-        this.downlaoder = builder.downloader;
-        this.properties = builder.properties;
-        this.options = builder.options;
-        this.urls = builder.urls;
+        this.properties.addAll(builder.properties);
+        this.options.putAll(builder.options);
+        this.urls.addAll(builder.urls);
+    }
+
+    private final void logged(String consoleLine) {
+        if (Objects.nonNull(consoleLogger)) {
+            consoleLogger.accept(consoleLine);
+        }
+    }
+
+    private final void terminated(List<String> consoleLog) {
+        if (Objects.nonNull(terminationLogger)) {
+            terminationLogger.accept(consoleLog);
+        }
     }
 
     @Override
-    public void logged(String consoleLine) {
-        consoleLogger.accept(consoleLine);
+    public final void run() {
+        Logger.getLogger(YoutubeDL.class.getName()).setLevel(Level.ALL);
+        try {
+            List<String> consoleLog = new ArrayList<>();
+            Process process = Runtime.getRuntime().exec(this.toString());
+            try (InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream())) {
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                for (String line = bufferedReader.readLine(); Objects.nonNull(line); line = bufferedReader.readLine()) {
+                    consoleLog.add(line);
+                    this.logged(line);
+                }
+                this.terminated(consoleLog);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(YoutubeDL.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void terminated(List<String> consoleLog) {
-        terminationLogger.accept(consoleLog);
+    public final String toString() {
+        String optionsString = options.entrySet().stream().map(entry -> String.join(" ", entry.getKey().toString(), entry.getValue())).collect(Collectors.joining(" "));
+        String propertiesString = properties.stream().map(property -> property.toString()).collect(Collectors.joining(" "));
+        return String.format("%s %s %s %s", executable, propertiesString, optionsString, String.join(" ", urls));
     }
 
-    @Override
-    public EnumMap<Options, String> getOptions() {
-        return options;
-    }
-
-    @Override
-    public EnumSet<Properties> getProperties() {
-        return properties;
-    }
-
-    @Override
-    public Optional<String> getDownloader() {
-        return Optional.of(downlaoder);
-    }
-
-    @Override
-    public List<String> getUrls() {
-        return urls;
-    }
-
-    public static class YoutubeDLBuilder {
-
-        private final EnumSet<Properties> properties = EnumSet.noneOf(Properties.class);
-        private final EnumMap<Options, String> options = new EnumMap(Options.class);
-        private final ArrayList<String> urls = new ArrayList<>();
-        private Consumer<List<String>> terminationLogger = null;
-        private Consumer<String> consoleLogger = null;
-        private String downloader = null;
-
-        public YoutubeDLBuilder terminationLogger(Consumer<List<String>> terminationLogger) {
-            this.terminationLogger = terminationLogger;
-            return this;
-        }
-
-        public YoutubeDLBuilder consoleLogger(Consumer<String> consoleLogger) {
-            this.consoleLogger = consoleLogger;
-            return this;
-        }
-
-        public YoutubeDLBuilder downloader(String downloader) {
-            this.downloader = downloader;
-            return this;
-        }
-
-        public YoutubeDLBuilder addOption(Options option, String value) {
-            options.put(option, value);
-            return this;
-        }
-
-        public YoutubeDLBuilder addProperty(Properties property) {
-            properties.add(property);
-            return this;
-        }
-
-        public YoutubeDLBuilder addURL(String url) {
-            urls.add(url);
-            return this;
-        }
-
-        public YoutubeDL build() {
-            return new YoutubeDL(this);
-        }
+    public static final void setExecutable(String executable) {
+        YoutubeDL.executable = executable;
     }
 }
